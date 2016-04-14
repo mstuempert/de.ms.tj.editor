c257228a79e4dec72b91561d646395c494187647
mathias <mathias@mathias-notebook>
2016-04-14 19:35:12 +0200

    forgotten

---
+++ de.ms.tj.editor/src/de/ms/tj/model/ISyntaxElementLibrary.java
@@ -0,0 +1,29 @@
+package de.ms.tj.model;
+
+public interface ISyntaxElementLibrary {
+	
+	public static final String CODE = "tj_syntax_code";
+	
+	public static final String COMMAND = "tj_syntax_command";
+	
+	public static final String COMMENT = "tj_syntax_comment";
+	
+	public static final String COMMENT_MULTI_LINE = "tj_syntax_comment_ml";
+	
+	public static final String COMMENT_SINGLE_LINE = "tj_syntax_comment_sl";
+	
+	public static final String STRING = "tj_syntax_string";
+	
+	public static final String STRING_MULTI_LINE = "tj_syntax_string_ml";
+	
+	public static final String STRING_SINGLE_LINE = "tj_syntax_string_sl";
+	
+	public static final String TYPE = "tj_syntax_type";
+	
+	public static final String TYPE_FLOAT = "tj_syntax_type_flt";
+	
+	public static final String TYPE_INTEGER = "tj_syntax_type_int";
+	
+	public static final String ROOT = "tj_syntax_root";
+
+}
